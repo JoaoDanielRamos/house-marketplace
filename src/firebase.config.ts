50302d87f81a1d6b1110adc492ac6d04da02d0ea
João Ramos <jdmor95@gmail.com>
2022-05-02 18:32:58 -0700

    adding starter files to the development branch. Setting up firebase and more.

---
+++ src/firebase.config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFom7TB4BeQWE84hWSZ7ZPYiog7ZqmLmE',
+  authDomain: 'house-marketplace-8972b.firebaseapp.com',
+  projectId: 'house-marketplace-8972b',
+  storageBucket: 'house-marketplace-8972b.appspot.com',
+  messagingSenderId: '39697870559',
+  appId: '1:39697870559:web:357960749d9ba286e5836a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
