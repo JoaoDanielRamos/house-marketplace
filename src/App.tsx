c7ecb98cd4439b2c59f0ebcc2ba7f4b21d5ec547
João Ramos <jdmor95@gmail.com>
2022-05-03 16:00:11 -0700

    added private route so it can check if user is logged in, if not redirect to sing in page. added spinner component to loading events. added custom hook to get the loggedIn status from fire base.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SingIn from './pages/SingIn';
 import SingUp from './pages/SingUp';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -19,7 +20,9 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SingIn />} />
           <Route path='/sign-up' element={<SingUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
