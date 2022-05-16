efe419c994e0053e040a0cf44c613454e74c141c
João Ramos <jdmor95@gmail.com>
2022-05-16 15:45:33 -0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import svgr from '@honkhonk/vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  optimizeDeps: {
+    include: ['swiper/react', 'swiper/types'],
+  },
 });
